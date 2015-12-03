262a73374b67e152324f79f1cf88f05e6d3ff1ee
Jesse <homestarflyer@gmail.com>
2015-12-03 22:45:01 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def initialize(name_st, breed_st = "Mut")
+    @name = name_st
+    @breed = breed_st
+  end
+  def name=(name_st)
+    @name = name_st
+  end
+  def name
+    @name
+  end
+  def breed=(breed_st)
+    @breed = breed_st
+  end
+  def breed
+    @breed
+  end
+end
